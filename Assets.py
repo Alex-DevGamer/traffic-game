a874cfa6fe62d0d6a24e3ea82d336f1b2167785b
Alex dos Santos <108100087+Alex-DevGamer@users.noreply.github.com>
2023-05-28 15:33:19 -0300

    Add files via upload
    Pre Carregamento de Imagens

---
+++ Assets.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import os
+class sprites():
+    pista = pg.image.load('imagens/star.png')
+    ia_car = pg.image.load('imagens/carroIA.png')
+    carro0 = pg.image.load('imagens/carro0.png')
+    carro1 = pg.image.load('imagens/carro1.png')
+    carro2 = pg.image.load('imagens/carro2.png')
+    carro3 = pg.image.load('imagens/carro3.png')
+    carro4 = pg.image.load('imagens/carro4.png')
+    carro5 = pg.image.load('imagens/carro5.png')
+    carro6 = pg.image.load('imagens/carro6.png')
+    carro_queimado = pg.image.load('imagens/carroQueimado.png')
+    carros = [carro0, carro1, carro2, carro3, carro4, carro5, carro6]
